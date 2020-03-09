e1cc2c1f0221b25475365ac4d9cec9614c7eab35
manan <mananman23@gmail.com>
2020-03-09 09:07:59 +0530

    Fixed bug #1

--- app/src/main/java/com/manan/admin/marvelavenger/MainActivity.java
+++ app/src/main/java/com/manan/admin/marvelavenger/MainActivity.java
@@ -1,9 +1,11 @@
 package com.manan.admin.marvelavenger;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdSize;
@@ -21,7 +23,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         //Ads
-        MobileAds.initialize(this, getString(R.string.appId_sample)); // ca-app-pub-5402594136802228~2621815489
+        MobileAds.initialize(this, getString(R.string.appId)); // ca-app-pub-5402594136802228~2621815489
         AdView adView = new AdView(this);
         adView.setAdSize(AdSize.BANNER);
         adView.setAdUnitId(getString(R.string.AdUnitId));
@@ -37,7 +39,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     //goto previous page when back key is pressed.
-    /*@Override
+    @Override
     public boolean onKeyDown(int keyCode, KeyEvent event) {
         if (event.getAction()==KeyEvent.ACTION_DOWN) {
             switch (keyCode) {
@@ -52,7 +54,7 @@ public class MainActivity extends AppCompatActivity {
             }
         }
         return super.onKeyDown(keyCode, event);
-    }*/
+    }
     /** Called when leaving the activity */
     @Override
     public void onPause() {
